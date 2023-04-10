27d91338476b95c09ad851157358c514dd6e3ffb
Elmehdi1O <mehdijavac@gmail.com>
2023-04-10 16:55:55 +0000

    Template Forms Part 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor() {}
+export class LoginComponent implements OnInit {
 
+  user = {username: '', password: '', remember: false};
+
+  constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('User: ', this.user);
+    this.dialogRef.close();
+  }
 
 }
