515feccc8929626fb13257e291bdb2edfa2f76c2
Elmehdi1O <mehdijavac@gmail.com>
2023-04-12 18:02:55 +0000

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,24 @@ import { Location } from '@angular/common';
 import { switchMap } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements  OnInit {
   dish: Dish | undefined;
@@ -26,6 +39,7 @@ export class DishdetailComponent implements  OnInit {
   };
   errMess!:string;
   dishcopy: Dish | undefined;
+  visibility = 'shown';
 
   validationMessages: { [key: string]: { [key: string]: string } }  = {
     'author': {
@@ -47,8 +61,8 @@ export class DishdetailComponent implements  OnInit {
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params.pipe(switchMap((params: Params)=>   this.dishservice.getDish(params['id'])))
-  .subscribe(dish => { this.dish = dish; this.dishcopy = dish, this.setPrevNext(dish.id); }, errMess => this.errMess = errMess);
+    this.route.params.pipe(switchMap((params: Params)=>  {  this.visibility = 'hidden'; return this.dishservice.getDish(params['id'])}))
+  .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown';  }, errMess => this.errMess = errMess);
     this.createFormComment();
   }
   previewShown!:boolean;
