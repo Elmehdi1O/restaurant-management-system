ee8b655b62b3d26b05a4f5069dc69d7993da4eae
Elmehdi1O <mehdijavac@gmail.com>
2023-04-14 14:24:55 +0000

    Assignment 4

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -1,21 +1,22 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Feedback } from "../shared/feedback";
-import { Observable } from "rxjs";
+import { Observable, catchError } from "rxjs";
 import { baseURL } from "../shared/baseurl";
+import { ProcessHTTPMsgService } from "./process-http-msg.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
   constructor(private http: HttpClient) { }
-  submitFeedback(data: Feedback): Observable<Feedback>{
+  submitFeedback(data: Feedback, processHTTPMsgService: ProcessHTTPMsgService ): Observable<Feedback>{
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     }
-    return this.http.post<Feedback>( baseURL + 'feedback', data, httpOptions);
+    return this.http.post<Feedback>( baseURL + 'feedback', data, httpOptions).pipe(catchError(processHTTPMsgService.handleError));
   }
 
 }
