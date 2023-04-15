99e356d7714475ad76c92ef855c1b33a64c161e8
Elmehdi1O <mehdijavac@gmail.com>
2023-04-15 18:07:09 +0000

    Component Test

--- src/test.ts
+++ src/test.ts
@@ -5,12 +5,12 @@ import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 // First, initialize the Angular testing environment.
+declare const  require: any
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
 
-// Then we find all the tests.
 const context = require.context('./', true, /menu\.component\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
